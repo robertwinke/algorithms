c6f0c2d4cf3fe622b68b5e9afe6ad83ec53f8b83
Robert Winke <robertwinke@gmail.com>
2017-07-09 20:56:51 +0200

    makes isprime check faster

--- projectEuler/010_summationOfPrimes/main.cpp
+++ projectEuler/010_summationOfPrimes/main.cpp
@@ -8,6 +8,7 @@
 
 
 #include <iostream>
+#include <vector>
 #include <cmath>
 
 using namespace std;
@@ -24,14 +25,31 @@ bool isPrime(int number)
   return true;
 }
 
+bool isPrime(int number, vector<int>& primes)
+{
+  for(int i = 0; primes[i] <= sqrt(number); ++i)
+  {
+    if(number % primes[i] == 0)
+    {
+      return false;
+    }
+  }
+  primes.push_back(number);
+  return true;
+}
+
 int main() {
+
+  vector<int> primes;
+  primes.push_back(2);
+
   long int sum = 2;
 
   long int number = 2000000;
 
   for (long int i = 3; i <= number; i += 2)
   {
-    if(isPrime(i))
+    if(isPrime(i, primes))
     {
       sum += i;
     }
